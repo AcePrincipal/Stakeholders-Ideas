db2a0f20f06771428fe905b3d7bc605c65020de3
Alexander <acepetrov96@gmail.com>
2021-02-22 11:18:30 -0600

    Iniialized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :ideas
-  resources :businesses
+  resources :businesses, only: [:index] do
+    resources :ideas, only: [:create, :index]
+  end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
