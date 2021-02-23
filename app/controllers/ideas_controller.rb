08e84478e74ac7ee1c7cc15b0bfae7d3bb4cce6e
Alexander <acepetrov96@gmail.com>
2021-02-23 16:16:05 -0600

    Added strong params, index, and create to ideas controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,19 @@
 class IdeasController < ApplicationController
+    def create
+        business = Business.find_by(id: params[:business_id])
+        idea = business.ideas.build(idea_params)
+        if idea.save
+            render json: idea
+        end
+    end
+
+    def index
+        ideas = Business.find_by(id: params[:business_id]).ideas
+        render json: ideas
+    end
+
+    private
+    def idea_params
+        params.require(:idea).permit(:title, :content)
+    end
 end
